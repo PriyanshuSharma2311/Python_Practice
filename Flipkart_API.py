6f6b91ea9df4dd5d5600c985268f715174872090
Priyanshu Sharma <78911880+PriyanshuSharma2311@users.noreply.github.com>
2022-02-03 09:34:29 +0530

    Add files via upload

---
+++ Flipkart_API.py
@@ -0,0 +1,19 @@
+import urllib.request as url
+import bs4
+
+query = input("Enter your query : ")
+query = query.replace(' ', '+')
+for i in range(1, 2):
+    print(f"Page : {i}")
+    response = url.urlopen(
+        'https://www.flipkart.com/search?q={}&page={}'.format(query, i))
+
+    page = bs4.BeautifulSoup(response,)
+    page.find('div', class_='_4rR01T')
+
+    titles = page.findAll('div', class_='_4rR01T')
+    priceList = page.findAll('div', class_='_30jeq3 _1_WHN1')
+for j in range(len(titles)):
+    print(titles[j].text)
+    print(priceList[j].text)
+    print('*' * 20)
